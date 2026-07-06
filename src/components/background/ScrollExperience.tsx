5664c838f2cc21b47a56cdd3cc487fb0758e8534
Muhammad Ahmed <ahmedmu7077@gmail.com>
2026-07-06 14:09:10 +0530

    feat: emphasize custom scroll progress

--- src/components/background/ScrollExperience.tsx
+++ src/components/background/ScrollExperience.tsx
@@ -5,5 +5,5 @@ import { motion, useScroll, useSpring } from "motion/react";
 export function ScrollExperience() {
   const { scrollYProgress } = useScroll();
   const progress = useSpring(scrollYProgress, { stiffness: 90, damping: 25, mass: .25 });
-  return <motion.div className="fixed left-0 top-0 z-[90] h-[3px] w-full origin-left bg-[#b86b4b]" style={{ scaleX: progress }} aria-hidden="true" />;
+  return <motion.div className="fixed left-0 top-0 z-[90] h-[5px] w-full origin-left bg-[#b86b4b] shadow-[0_0_10px_rgba(184,107,75,.55)]" style={{ scaleX: progress }} aria-hidden="true" />;
 }
